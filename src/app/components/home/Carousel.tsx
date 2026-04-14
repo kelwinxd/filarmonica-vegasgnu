2be8df451d41984d107ecff6c556b8080b794182
Kelwin Esechiel <kelwin_esechiel@hotmail.com>
2026-04-14 17:41:45 -0300

    text slide

--- src/app/components/home/Carousel.tsx
+++ src/app/components/home/Carousel.tsx
@@ -15,11 +15,13 @@ const slides = [
     id: 1,
     image: "/banner01.png",
     title: "Evento 1",
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem saepe nobis commodi id eius illum, labore omnis unde reprehenderit perspiciatis!"
   },
   { 
     id: 2,
     image: "/slide02.png",
     title: "Evento 2",
+    desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem saepe nobis commodi id eius illum, labore omnis unde reprehenderit perspiciatis!"
   },
 ];
 
@@ -36,9 +38,10 @@ export default function Carousel() {
         pagination={{ clickable: true }}
        
     >
+      
       {slides.map((slide) => (
         <SwiperSlide key={slide.id}>
-          <div className="relative w-full overflow-hidden h-[500px] md:h-screen">
+          <div className="relative w-full overflow-hidden h-[500px] md:h-screen ">
               <Image
     src={slide.image}
     alt={slide.title}
@@ -47,10 +50,15 @@ export default function Carousel() {
     priority
     className="object-cover object-top"
   />
-
-            <h2 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center text-4xl">
+          <div className="flex flex-col gap-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+           <h2 className=" text-white text-center text-4xl font-title font-medium uppercase">
               {slide.title}
             </h2>
+            <p className=" text-white text-center text-[16px] font-fam-parag">
+              {slide.desc}
+            </p>
+          </div>
+           
           </div>
         </SwiperSlide>
       ))}
