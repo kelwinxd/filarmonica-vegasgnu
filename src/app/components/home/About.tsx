b8e4c198dc6385e642b2e078fd07a6415dae3358
Kelwin Esechiel <kelwin_esechiel@hotmail.com>
2026-04-15 12:35:37 -0300

    fix: imagem about

--- src/app/components/home/About.tsx
+++ src/app/components/home/About.tsx
@@ -23,65 +23,65 @@ const About = () => {
         </p>
       </div>
 
-       {/* Story */}
-      <div className="mt-[72px] flex flex-col md:flex-row gap-6 ">
-        <div className="flex-1 ">
-          <p className="text-subparag text-[20px] font-medium">Fundado em junho de 2022, o projeto nasceu da parceria entre o Espaço Gnu e a Vegas Card, com o objetivo de ampliar o acesso à educação musical e gerar impacto social por meio da cultura. Ao longo de sua atuação, a iniciativa vem se consolidando como um espaço de aprendizado, inclusão e desenvolvimento, atendendo crianças, adolescentes e adultos em um ambiente estruturado e acolhedor</p>
+        {/* Story */}
+        <div className="mt-[72px] flex flex-col md:flex-row gap-6 ">
+          <div className="flex-1 ">
+            <p className="text-subparag text-[20px] font-medium">Fundado em junho de 2022, o projeto nasceu da parceria entre o Espaço Gnu e a Vegas Card, com o objetivo de ampliar o acesso à educação musical e gerar impacto social por meio da cultura. Ao longo de sua atuação, a iniciativa vem se consolidando como um espaço de aprendizado, inclusão e desenvolvimento, atendendo crianças, adolescentes e adultos em um ambiente estruturado e acolhedor</p>
 
-          <div className="flex flex-col md:flex-row gap-6 mt-[35px]">
-            <Button variant="secondary" text="dark" border="outline">
-              Quero ser Parceiro
-            </Button>
-             <Button variant="primary" text="light">
-              Quero me inscrever
-            </Button>
+            <div className="flex flex-col md:flex-row gap-6 mt-[35px]">
+              <Button variant="secondary" text="dark" border="outline">
+                Quero ser Parceiro
+              </Button>
+              <Button variant="primary" text="light">
+                Quero me inscrever
+              </Button>
+            </div>
+          </div>
+
+          <div className="flex-1 relative min-h-[320px] h-[320px] md:h-[380px]">
+            {/*  Imagem */}
+            <Image src="/aboutimg.png" className="object-cover rounded-[8px]" fill alt="imagem about" />
           </div>
         </div>
 
-        <div className="flex-1 relative">
-          {/*  Imagem */}
-          <Image src="/aboutimg.png" className="object-cover min-h-[300px] max-h-[320px] rounded-[8px]" fill alt="imagem about" />
-        </div>
-      </div>
+        {/* About Cards */}
+        <div className="mt-[120px]  mb-[120px] flex flex-col md:flex-row gap-6">
+          <AboutCard title="+100">
+            <h2 className="text-[16px] md:text-[20px] text-main font-bold">
+              Alunos atendidos
+            </h2>
+            <p className="text-subparag text-[16px] md:text-[20px] leading-6 mt-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dolor.
+            </p>
+          </AboutCard>
 
-      {/* About Cards */}
-      <div className="mt-50 md:mt-[120px]  mb-[120px] flex flex-col md:flex-row gap-6">
-        <AboutCard title="+100">
+            <AboutCard title="+2 Anos">
           <h2 className="text-[16px] md:text-[20px] text-main font-bold">
-            Alunos atendidos
-          </h2>
-          <p className="text-subparag text-[16px] md:text-[20px] leading-6 mt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dolor.
-          </p>
-        </AboutCard>
+              Alunos atendidos
+            </h2>
+            <p className="text-subparag text-[16px] md:text-[20px] leading-6 mt-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dolor.
+            </p>
+          </AboutCard>
 
-          <AboutCard title="+2 Anos">
-         <h2 className="text-[16px] md:text-[20px] text-main font-bold">
-            Alunos atendidos
-          </h2>
-          <p className="text-subparag text-[16px] md:text-[20px] leading-6 mt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dolor.
-          </p>
-        </AboutCard>
+            <AboutCard title="+50">
+            <h2 className="text-[16px] md:text-[20px] text-main font-bold">
+              Alunos atendidos
+            </h2>
+            <p className="text-subparag text-[16px] md:text-[20px] leading-6 mt-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dolor.
+            </p>
+          </AboutCard>
 
-          <AboutCard title="+50">
-           <h2 className="text-[16px] md:text-[20px] text-main font-bold">
-            Alunos atendidos
-          </h2>
-          <p className="text-subparag text-[16px] md:text-[20px] leading-6 mt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dolor.
-          </p>
-        </AboutCard>
-
-          <AboutCard title="100%">
-           <h2 className="text-[16px] md:text-[20px] text-main font-bold">
-            Alunos atendidos
-          </h2>
-          <p className="text-subparag text-[16px] md:text-[20px] leading-6 mt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dolor.
-          </p>
-        </AboutCard>
-      </div>
+            <AboutCard title="100%">
+            <h2 className="text-[16px] md:text-[20px] text-main font-bold">
+              Alunos atendidos
+            </h2>
+            <p className="text-subparag text-[16px] md:text-[20px] leading-6 mt-2">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, dolor.
+            </p>
+          </AboutCard>
+        </div>
 
      
 
